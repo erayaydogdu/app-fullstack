0e9e763eb749a13ed0f40143a755e7b23a044717
erayaydogdu <eray.aydogdu@outlook.com>
2025-04-14 01:43:59 +0200

    change the nextjs sign in page

--- src/apps/nextjs-ui/src/app/auth/sign-in/[[...sign-in]]/page.tsx
+++ src/apps/nextjs-ui/src/app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from 'next';
-import { GalleryVerticalEnd } from "lucide-react"
+import { Fan } from "lucide-react"
 import { SignInForm } from "@/features/auth/components/sign-in-form"
 
 export const metadata: Metadata = {
@@ -13,7 +13,7 @@ export default function LoginPage() {
       <div className="flex w-full max-w-sm flex-col gap-6">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-            <GalleryVerticalEnd className="size-4" />
+            <Fan className="size-4" />
           </div>
           The App
         </a>
